da14ad0ac758755c41230ff0e9fbf1fa646b7244
andresvacca <andresfvacca2019@gmail.com>
2026-08-17 22:56:17 -0500

    Update models.py

--- modules/inventario/infrastructure/django/models.py
+++ modules/inventario/infrastructure/django/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Articulos(models.Model):
     nombre = models.CharField(max_length=150)
     precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_compra = models.DecimalField(10, 2)
+    stock = models.IntegerField(max_length=100)
+    

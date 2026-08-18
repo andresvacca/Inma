8ebc7e6a4408f072eff93cd1875e5786b8610704
andresvacca <andresfvacca2019@gmail.com>
2026-08-17 21:35:35 -0500

    Reseteo

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,11 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
-    'core',
-    'sales',
-    'purchases',
-    'products',
+    
 ]
 
 MIDDLEWARE = [
